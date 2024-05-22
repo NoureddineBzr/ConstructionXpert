8aca384c9b1f460ce01a7ed5c3033ca3e032bc60
Noureddine Bouzire <bouzire.noureddine1@gmail.com>
2024-05-22 10:30:38 +0100

    creating the DAO Resource

---
+++ ConstructionXpertApp/src/main/java/servlets/ResourceServlet.java
@@ -0,0 +1,18 @@
+package servlets;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ResourceServlet", value = "/ResourceServlet")
+public class ResourceServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
